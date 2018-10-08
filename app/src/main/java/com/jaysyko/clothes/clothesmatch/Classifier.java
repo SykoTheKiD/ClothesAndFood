8f36fc4549f9b7cebf7d45223a2e6e7a8ceb66b0
Jay Syko <jay@jaysyko.com>
2018-10-08 15:14:43 -0400

    initial

---
+++ app/src/main/java/com/jaysyko/clothes/clothesmatch/Classifier.java
@@ -0,0 +1,70 @@
+package com.jaysyko.clothes.clothesmatch;
+
+import android.graphics.Bitmap;
+import android.support.annotation.NonNull;
+
+import java.util.List;
+
+public interface Classifier {
+
+    class Recognition {
+        /**
+         * A unique identifier for what has been recognized. Specific to the class, not the instance of
+         * the object.
+         */
+        private final String id;
+
+        /**
+         * Display name for the recognition.
+         */
+        private final String title;
+
+        /**
+         * A sortable score for how good the recognition is relative to others. Higher should be better.
+         */
+        private final Float confidence;
+
+        Recognition(
+                final String id, final String title, final Float confidence) {
+            this.id = id;
+            this.title = title;
+            this.confidence = confidence;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        Float getConfidence() {
+            return confidence;
+        }
+
+        @NonNull
+        @Override
+        public String toString() {
+            String resultString = "";
+            if (id != null) {
+                resultString += "[" + id + "] ";
+            }
+
+            if (title != null) {
+                resultString += title + " ";
+            }
+
+            if (confidence != null) {
+                resultString += String.format("(%.1f%%) ", confidence * 100.0f);
+            }
+
+            return resultString.trim();
+        }
+    }
+
+
+    List<Recognition> recognizeImage(Bitmap bitmap);
+
+    void close();
+}
